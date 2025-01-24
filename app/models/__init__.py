993aeb89a7053cafcf57cbe13acbd1a1b16d23ce
GinGa-Dza <nakedharmonix@gmail.com>
2025-01-24 14:54:01 +0900

    モデル層: app/models/__init__.py の修正を適用

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,7 @@
 # app/models/__init__.py
+# noqa: F401
 from .base import Base
 from .group import Group
 from .skill_matrix import SkillMatrix
 from .skill import Skill
-from .user import User  # Userモデルをインポート
+from .user import User # Userモデルをインポート
